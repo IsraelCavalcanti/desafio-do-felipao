c5783f70f26afeb2b61c6b51dafbb5131ed230b7
IsraelCavalcanti <israelcavalcanti.br@gmail.com>
2024-06-16 14:50:40 -0300

    desafio

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
-let nome = "Israel";
-console.log(nome);
+var nome = prompt('Digite seu nome: ');
+var nivel;
+var xp;
+
+if xp < 1000 {
+    nivel = "Ferro";
+} 
+if xp => 1001 && < 2001 {
+
+} 
+
+console.log("O herói de nome " + nome + " está no nível de " nivel);
